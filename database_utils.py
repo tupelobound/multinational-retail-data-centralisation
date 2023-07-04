3673bc7333232246e9fc462c4d1e93b62b7261a7
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-04 18:31:54 +0100

    Adds read_db_creds method

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,11 @@
+import yaml
+from sqlalchemy import create_engine
+
 class DatabaseConnector:
-    pass
+    def read_db_creds(self):
+        '''Opens the YAML file containing the database credentials and returns the credentials as a dictionary'''
+        # Use context manager to open file
+        with open('db_creds.yaml', 'r') as file:
+            # load contents into dictionary and return
+            return yaml.safe_load(file)
+    
