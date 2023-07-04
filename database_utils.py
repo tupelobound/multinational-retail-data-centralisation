84e7bc898cdee5a8bcf25aa234aebf78609bf076
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-04 18:32:39 +0100

    Adds init_db_engine method

--- database_utils.py
+++ database_utils.py
@@ -9,3 +9,14 @@ class DatabaseConnector:
             # load contents into dictionary and return
             return yaml.safe_load(file)
     
+
+    def init_db_engine(self):
+        db_credentials = self.read_db_creds()
+        engine = create_engine(f"postgresql+psycopg2://{db_credentials['RDS_USER']}:{db_credentials['RDS_PASSWORD']} \
+                               @{db_credentials['RDS_HOST']}:{db_credentials['RDS_PORT']}/{db_credentials['RDS_DATABASE']}")
+        
+        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+
+
+test = DatabaseConnector()
+test.init_db_engine()
