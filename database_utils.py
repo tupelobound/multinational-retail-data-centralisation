67632209b15d0b67a570e8a7b52ccb22fbf8ba61
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-05 05:14:59 +0100

    Fixes init_db_engine() method

--- database_utils.py
+++ database_utils.py
@@ -1,9 +1,10 @@
 import yaml
 from sqlalchemy import create_engine
 
+
 class DatabaseConnector:
     def read_db_creds(self):
-        '''Opens the YAML file containing the database credentials and returns the credentials as a dictionary'''
+        '''Opens the YAML file containing the database credentials and returns the credentials as a dictionary.'''
         # Use context manager to open file
         with open('db_creds.yaml', 'r') as file:
             # load contents into dictionary and return
@@ -11,12 +12,15 @@ class DatabaseConnector:
     
 
     def init_db_engine(self):
+        '''Creates a connection string of database credentials and uses that string to create and return sqlalchemy database engine.'''
+        # Call read_db_creds() method to get database credentials as dictionary
         db_credentials = self.read_db_creds()
-        engine = create_engine(f"postgresql+psycopg2://{db_credentials['RDS_USER']}:{db_credentials['RDS_PASSWORD']} \
-                               @{db_credentials['RDS_HOST']}:{db_credentials['RDS_PORT']}/{db_credentials['RDS_DATABASE']}")
-        
-        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+        # Construct connection string using contents of dictionary
+        connection_string = f"postgresql+psycopg2://{db_credentials['RDS_USER']}:{db_credentials['RDS_PASSWORD']}@" + \
+                            f"{db_credentials['RDS_HOST']}:{db_credentials['RDS_PORT']}/{db_credentials['RDS_DATABASE']}"
+        # Create new sqlalchemy database engine and return
+        return create_engine(connection_string)
 
 
 test = DatabaseConnector()
-test.init_db_engine()
+print(test.list_db_tables())
