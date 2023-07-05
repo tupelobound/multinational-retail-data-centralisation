b6e8a5fd5f49784ee7f211254c62fad72cade82a
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-05 06:02:42 +0100

    Adds read_rds_table() method

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,12 @@
+from database_utils import DatabaseConnector
+import pandas as pd
+
 class DataExtractor:
-    pass
+    def read_rds_table(self, connector, table):
+        engine = connector().init_db_engine()
+        return pd.read_sql_table(table, engine)
+
+
+if __name__ == "__main__":
+    test = DataExtractor()
+    print(test.read_rds_table(DatabaseConnector, 'legacy_users').info())
