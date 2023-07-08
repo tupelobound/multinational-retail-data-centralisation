22a96bd6e45bf45367beb42134bc0e1a5a700089
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-08 16:20:18 +0100

    Fixes DataConnector initialisation in read_rds_table()

--- data_extraction.py
+++ data_extraction.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 class DataExtractor:
     def read_rds_table(self, connector, table):
-        engine = connector().init_db_engine()
+        engine = connector.init_db_engine()
         return pd.read_sql_table(table, engine)
