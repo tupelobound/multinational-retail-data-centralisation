677d4b87efeb8241a4d5948e8111e80972ad7ad6
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-12 23:28:07 +0100

    Updates data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -15,4 +15,15 @@ class DataCleaning:
         users['date_of_birth'] = pd.to_datetime(users['date_of_birth'])
         # convert join date column to datetime type
         users['join_date'] = pd.to_datetime(users['join_date'])
+        # drop users whose birthday is later than their join date
+        users.drop(users[users['date_of_birth'] > users['join_date']].index, inplace=True)
+        # correct 'GGB' values in country code column
+        users['country_code'] = users['country_code'].apply(lambda x: 'GB' if x == 'GGB' else x)
+        # convert country column to categorical data type
+        users['country'] = users['country'].astype('category')
+        # convert country code column to categorical data type
+        users['country_code'] = users['country_code'].astype('category')
+
+        print(users.info())
+
         return users
