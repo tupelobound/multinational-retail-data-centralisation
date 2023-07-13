23810d2d14f9f47794ab73211d85911e2bedbf3c
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-13 23:04:59 +0100

    Adds retrieve_pdf_data() method to DataExtractor class

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import tabula
 
 class DataExtractor:
     def read_rds_table(self, connector, table):
         engine = connector.init_db_engine()
         return pd.read_sql_table(table, engine)
+    
+    def retrieve_pdf_data(self, link):
+        return pd.concat(tabula.read_pdf(link, pages='all'))
