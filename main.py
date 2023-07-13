557929bf674caaa933c5c563377571f7c310309c
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-13 21:58:58 +0100

    Changes test code to include upload to local db

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from database_utils import DatabaseConnector
 from data_extraction import DataExtractor
 from data_cleaning import DataCleaning
 
-connector = DatabaseConnector()
+aws_connector = DatabaseConnector('aws_creds.yaml')
+local_connector = DatabaseConnector('local_creds.yaml')
 extractor = DataExtractor()
 cleaner = DataCleaning()
 
-users = extractor.read_rds_table(connector, 'legacy_users')
-cleaner.clean_user_data(users).to_csv('users.csv')
+users = extractor.read_rds_table(aws_connector, 'legacy_users')
+local_connector.upload_to_db(cleaner.clean_user_data(users), 'dim_users')
 
