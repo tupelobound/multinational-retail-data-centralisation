a1ba5b129b6e2e520d8c155ac9245183143cbe1d
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-13 23:05:58 +0100

    Adds logic for extracting and uploading card data

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ extractor = DataExtractor()
 cleaner = DataCleaning()
 
 users = extractor.read_rds_table(aws_connector, 'legacy_users')
+cards = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
 local_connector.upload_to_db(cleaner.clean_user_data(users), 'dim_users')
+local_connector.upload_to_db(cards, 'dim_card_details')
 
