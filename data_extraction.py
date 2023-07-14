20d53069b8e2e475104d4a2695bb3188a61fdb81
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-14 21:34:59 +0100

    Adds list_number_of_stores() method to DataExtractor class

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tabula
+import requests
 
 class DataExtractor:
     def read_rds_table(self, connector, table):
@@ -8,3 +9,7 @@ class DataExtractor:
     
     def retrieve_pdf_data(self, link):
         return pd.concat(tabula.read_pdf(link, pages='all'))
+    
+    def list_number_of_stores(self, endpoint, header_dictionary):
+        response = requests.get(endpoint, headers=header_dictionary)
+        return response.json()['number_stores']
