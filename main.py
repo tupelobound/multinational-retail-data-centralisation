47d1fdcece8782cdea442befb02d94ef0f446a95
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-14 21:36:33 +0100

    Adds code for testing list_number_of_stores method

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ local_connector = DatabaseConnector('local_creds.yaml')
 extractor = DataExtractor()
 cleaner = DataCleaning()
 
-users = extractor.read_rds_table(aws_connector, 'legacy_users')
-cards = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
-local_connector.upload_to_db(cleaner.clean_user_data(users), 'dim_users')
-local_connector.upload_to_db(cleaner.clean_card_data(cards), 'dim_card_details')
+#users = extractor.read_rds_table(aws_connector, 'legacy_users')
+#cards = extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+#local_connector.upload_to_db(cleaner.clean_user_data(users), 'dim_users')
+#local_connector.upload_to_db(cleaner.clean_card_data(cards), 'dim_card_details')
+
+header = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+print(extractor.list_number_of_stores(endpoint, header))
 
