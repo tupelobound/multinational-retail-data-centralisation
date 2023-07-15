4ad452ffa577d4e378b8a32c09ecb472418717a0
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-15 08:16:58 +0100

    Adds retrieve_stores_data() method to DataExtractor class

--- data_extraction.py
+++ data_extraction.py
@@ -3,6 +3,11 @@ import tabula
 import requests
 
 class DataExtractor:
+    def __init__(self):
+        self.number_of_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+        self.get_store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
+        self.api_header = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+
     def read_rds_table(self, connector, table):
         engine = connector.init_db_engine()
         return pd.read_sql_table(table, engine)
@@ -10,6 +15,14 @@ class DataExtractor:
     def retrieve_pdf_data(self, link):
         return pd.concat(tabula.read_pdf(link, pages='all'))
     
-    def list_number_of_stores(self, endpoint, header_dictionary):
-        response = requests.get(endpoint, headers=header_dictionary)
+    def list_number_of_stores(self):
+        response = requests.get(self.number_of_stores_endpoint, headers=self.api_header)
         return response.json()['number_stores']
+    
+    def retrieve_stores_data(self):
+        stores = pd.DataFrame()
+        for store_number in range(0, self.list_number_of_stores()):
+            response = requests.get(self.get_store_endpoint + str(store_number), headers=self.api_header)
+            new_dataframe = pd.DataFrame(response.json(), index=[0])
+            stores = pd.concat([stores, new_dataframe])
+        return stores
