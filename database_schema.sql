ad73cf89bea2612899bcfa99a697f0c1252179bf
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-07-22 21:54:53 +0100

    Adds comments to database_schema.sql

--- database_schema.sql
+++ database_schema.sql
@@ -1,3 +1,5 @@
+
+-- Task 1. Cast the columns of the orders_table to the correct data types.
 ALTER TABLE orders_table
     ALTER COLUMN user_uuid TYPE UUID USING user_uuid::UUID,
     ALTER COLUMN date_uuid TYPE UUID USING date_uuid::UUID,
@@ -6,6 +8,7 @@ ALTER TABLE orders_table
     ALTER COLUMN product_code TYPE VARCHAR(11),
     ALTER COLUMN product_quantity TYPE SMALLINT;
 
+-- Task 2. Cast the columns of the dim_users table to the correct data types.
 ALTER TABLE dim_users
     ALTER COLUMN first_name TYPE VARCHAR(255),
     ALTER COLUMN last_name TYPE VARCHAR(255),
@@ -15,6 +18,7 @@ ALTER TABLE dim_users
     ALTER COLUMN join_date TYPE DATE,
     ADD PRIMARY KEY (user_uuid);
 
+-- Task 3. Update the dim_store_details table.
 ALTER TABLE dim_store_details
     ALTER COLUMN longitude TYPE FLOAT USING longitude::FLOAT,
     ALTER COLUMN locality TYPE VARCHAR(255),
@@ -28,12 +32,14 @@ ALTER TABLE dim_store_details
     ALTER COLUMN continent TYPE VARCHAR(255),
     ADD PRIMARY KEY (store_code);
 
+-- Task 4. Make changes to the dim_products table for the delivery team.
 UPDATE dim_products
     SET product_price = LTRIM(product_price, '£');
 
 ALTER TABLE dim_products
     RENAME removed TO still_available;
 
+-- Task 5. Update the dim_products table with the required data types.
 ALTER TABLE dim_products
     ADD COLUMN weight_class VARCHAR(14),
     ALTER COLUMN product_price TYPE FLOAT USING product_price::FLOAT,
@@ -54,6 +60,7 @@ UPDATE dim_products
             WHEN weight >= 140.0 THEN 'Truck_Required'
         END;
 
+-- Task 6. Update the dim_date_times table.
 ALTER TABLE dim_date_times
     ALTER COLUMN date_uuid TYPE UUID USING date_uuid::UUID,
     ALTER COLUMN month TYPE VARCHAR(2),
@@ -62,12 +69,15 @@ ALTER TABLE dim_date_times
     ALTER COLUMN time_period TYPE VARCHAR(10),
     ADD PRIMARY KEY (date_uuid);
 
+-- Task 7. Update the dim_card_details table
 ALTER TABLE dim_card_details
     ALTER COLUMN card_number TYPE VARCHAR(19),
     ALTER COLUMN expiry_date TYPE VARCHAR(5),
     ALTER COLUMN date_payment_confirmed TYPE DATE,
     ADD PRIMARY KEY (card_number);
 
+-- Task 8. Create the primary keys in the dimension table - above
+-- Task 9. Add foreign keys to the orders table
 ALTER TABLE orders_table
     ADD FOREIGN KEY (date_uuid) REFERENCES dim_date_times(date_uuid),
     ADD FOREIGN KEY (user_uuid) REFERENCES dim_users(user_uuid),
